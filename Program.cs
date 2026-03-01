bb2c3e9feea242ad29536197d240b86dd242b23d
emanu <emanu@EMANUEL_RAMREZ>
2026-02-28 19:01:18 -0600

    Logica aplicada!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿//Mostrar la tabla de multiplicar de un número ingresado.
 
+Console.WriteLine("Ingrese un número");
+int num = int.Parse(Console.ReadLine());
+
+int contador = 0;
+
+while (contador <= 10)
+{
+    Console.WriteLine(num + " X "+ contador + " = " + (num * contador));
+    contador++;
+}
 
